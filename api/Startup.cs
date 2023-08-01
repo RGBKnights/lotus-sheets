43f54a8eee34bc64fe066a990cfbd8ceb2a7b87a
RGBKnights <master@rgbknights.com>
2023-08-01 00:20:17 -0400

    Added static site and functions for testing

---
+++ api/Startup.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Lotus.Sheets.Startup))]
+
+namespace Lotus.Sheets
+{
+  public class Startup : FunctionsStartup
+  {
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+      var context = builder.GetContext();
+
+      builder.ConfigurationBuilder
+        .AddJsonFile(Path.Combine(context.ApplicationRootPath, "local.settings.json"), optional: true, reloadOnChange: false)
+        .AddUserSecrets("def4c678-2c7c-440f-9747-6c8e6b009d88")
+        .AddEnvironmentVariables();
+    }
+
+    override public void Configure(IFunctionsHostBuilder builder)
+    {
+      builder.Services.AddHttpClient();
+    }
+  }
+}
